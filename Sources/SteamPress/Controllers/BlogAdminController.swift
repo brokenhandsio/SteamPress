cffda83f745f8e87cbcf86e22ef67bb40233b632
Tim <0xtimc@gmail.com>
2017-01-13 00:14:53 +0000

    Use Vapor's Random so it will compile on Linux

--- Sources/SteamPress/Controllers/BlogAdminController.swift
+++ Sources/SteamPress/Controllers/BlogAdminController.swift
@@ -556,7 +556,7 @@ extension String {
         var randomString: String = ""
         
         for _ in 0..<length {
-            let randomValue = arc4random_uniform(UInt32(base.characters.count))
+            let randomValue = Int.random(min: 0, max: base.characters.count)
             randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
         }
         return randomString
