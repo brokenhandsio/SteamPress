65b159338de63c6c6274225827fbcfaa79d65d33
Tim <0xtimc@gmail.com>
2017-01-26 13:07:42 +0000

    Use any Vapor 1.X release

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SteamPress",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 4),
+    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
     	.Package(url: "https://github.com/brokenhandsio/LeafMarkdown.git", majorVersion: 0, minor: 1)
     ]
 )
