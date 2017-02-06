0dc83e3b7d0677eb43a131a0976e34284827d13c
Tim <0xtimc@gmail.com>
2017-02-06 12:45:06 +0000

    Added all the failing tests I think I need to test slugify

--- Tests/SteamPressTests/BlogPostTests.swift
+++ Tests/SteamPressTests/BlogPostTests.swift
@@ -9,14 +9,64 @@ class BlogPostTests: XCTestCase {
     ]
     
     func testThatSlugUrlCalculatedCorrectlyForTitleWithSpaces() {
-        
         let title = "This is a title"
         let expectedSlugUrl = "this-is-a-title"
-        
-        let post = BlogPost(title: "My title", contents: "<p>This is a blog post</p>", author: BlogUser(name: "A user", username: "Username", password: "password"), creationDate: Date(), slugUrl: title)
-        
+        let post = TestDataBuilder.anyPost(slugUrl: title)
         XCTAssertEqual(expectedSlugUrl, post.slugUrl)
         
     }
     
+    func testThatSlugUrlCalculatedCorrectlyForTitleWithPunctuation() {
+        let title = "This is an awesome post!"
+        let expectedSlugUrl = "this-is-an-awesome-post"
+        let post = TestDataBuilder.anyPost(slugUrl: title)
+        XCTAssertEqual(expectedSlugUrl, post.slugUrl)
+    }
+    
+    func testThatSlugUrlNotChangedWhenSetWithValidSlugUrl() {
+        let slugUrl = "this-is-a-title"
+        let post = TestDataBuilder.anyPost(slugUrl: slugUrl)
+        XCTAssertEqual(slugUrl, post.slugUrl)
+    }
+    
+    func testThatSlugUrlStripsWhitespace() {
+        let title = "    Title  "
+        let expectedSlugUrl = "title"
+        let post = TestDataBuilder.anyPost(slugUrl: title)
+        XCTAssertEqual(expectedSlugUrl, post.slugUrl)
+    }
+    
+    func testNumbersRemainInUrl() {
+        let title = "The 2nd url"
+        let expectedSlugUrl = "the-2nd-url"
+        let post = TestDataBuilder.anyPost(slugUrl: title)
+        XCTAssertEqual(expectedSlugUrl, post.slugUrl)
+    }
+    
+    func testSlugUrlLowerCases() {
+        let title = "AN AMAZING POST"
+        let expectedSlugUrl = "an-amazing-post"
+        let post = TestDataBuilder.anyPost(slugUrl: title)
+        XCTAssertEqual(expectedSlugUrl, post.slugUrl)
+    }
+    
+    func testEverythingWithLotsOfCharacters() {
+        let title = " This will be Amazing! Can't wait to @ you for $$££€ "
+        let expectedSlugUrl = "this-will-be-amazing-cant-wait-to-you-for"
+        let post = TestDataBuilder.anyPost(slugUrl: title)
+        XCTAssertEqual(expectedSlugUrl, post.slugUrl)
+    }
+    
+    // TODO test snippets
+    
+}
+
+struct TestDataBuilder {
+    static func anyUser() -> BlogUser {
+        return BlogUser(name: "Tim C", username: "timc", password: "password")
+    }
+    
+    static func anyPost(slugUrl: String = "some-exciting-title")  -> BlogPost {
+        return BlogPost(title: "An Exciting Post!", contents: "<p>This is a blog post</p>", author: anyUser(), creationDate: Date(), slugUrl: slugUrl)
+    }
 }
