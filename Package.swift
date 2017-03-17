897f4dd8ed0c20d1684776af881e6574a14448fc
Tim <0xtimc@gmail.com>
2017-03-17 15:40:28 +0000

    Back to main SwiftSoup as my changes have been merged in

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SteamPress",
     dependencies: [
     	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-    	.Package(url: "https://github.com/brokenhandsio/SwiftSoup.git", majorVersion: 1),
+    	.Package(url: "https://github.com/scinfu/SwiftSoup.git", majorVersion: 1),
     	.Package(url: "https://github.com/brokenhandsio/LeafMarkdown.git", majorVersion: 0),
     	.Package(url: "https://github.com/nodes-vapor/paginator", majorVersion: 0)
     ]
