c639f8b48af5bd2d2e865985145dfaed090cf1e7
Tim <0xtimc@gmail.com>
2017-03-17 11:54:06 +0000

    Add tests for Blog Post and Blog Index

--- Tests/SteamPressTests/LeafViewFactoryTests.swift
+++ Tests/SteamPressTests/LeafViewFactoryTests.swift
@@ -18,13 +18,27 @@ class LeafViewFactoryTests: XCTestCase {
         ("testNoTagsGivenIfEmptyArrayPassedToAllTagsPage", testNoTagsGivenIfEmptyArrayPassedToAllTagsPage),
         ("testParametersAreSetCorrectlyOnAllAuthorsPage", testParametersAreSetCorrectlyOnAllAuthorsPage),
         ("testAuthorsPageGetsPassedAllAuthorsWithBlogCount", testAuthorsPageGetsPassedAllAuthorsWithBlogCount),
+        ("testAuthorsPageGetsPassedAuthorsSortedByPageCount", testAuthorsPageGetsPassedAuthorsSortedByPageCount),
         ("testTwitterHandleSetOnAllAuthorsPageIfProvided", testTwitterHandleSetOnAllAuthorsPageIfProvided),
         ("testNoLoggedInUserPassedToAllAuthorsPageIfNoneProvided", testNoLoggedInUserPassedToAllAuthorsPageIfNoneProvided),
+        ("testNoAuthorsGivenToAuthorsPageIfNonePassedToAllAuthorsPage", testNoAuthorsGivenToAuthorsPageIfNonePassedToAllAuthorsPage),
         ("testTagPageGetsTagWithCorrectParamsAndPostCount", testTagPageGetsTagWithCorrectParamsAndPostCount),
         ("testNoLoggedInUserPassedToTagPageIfNoneProvided", testNoLoggedInUserPassedToTagPageIfNoneProvided),
         ("testDisqusNamePassedToTagPageIfSet", testDisqusNamePassedToTagPageIfSet),
         ("testTwitterHandlePassedToTagPageIfSet", testTwitterHandlePassedToTagPageIfSet),
-        ("testBlogPageGetsImageUrlIfOneInPostMarkdown", testBlogPageGetsImageUrlIfOneInPostMarkdown)
+        ("testBlogPageGetsImageUrlIfOneInPostMarkdown", testBlogPageGetsImageUrlIfOneInPostMarkdown),
+        ("testDescriptionOnBlogPostPageIsShortSnippetTextCleaned", testDescriptionOnBlogPostPageIsShortSnippetTextCleaned),
+        ("testBlogPostPageGetsCorrectParameters", testBlogPostPageGetsCorrectParameters),
+        ("testUserPassedToBlogPostPageIfUserPassedIn", testUserPassedToBlogPostPageIfUserPassedIn),
+        ("testDisqusNamePassedToBlogPostPageIfPassedIn", testDisqusNamePassedToBlogPostPageIfPassedIn),
+        ("testTwitterHandlePassedToBlogPostPageIfPassedIn", testTwitterHandlePassedToBlogPostPageIfPassedIn),
+        ("testBlogIndexPageGivenCorrectParameters", testBlogIndexPageGivenCorrectParameters),
+        ("testNoPostsPassedIntoBlogIndexIfNoneAvailable", testNoPostsPassedIntoBlogIndexIfNoneAvailable),
+        ("testNoAuthorsPassedIntoBlogIndexIfNoneCreated", testNoAuthorsPassedIntoBlogIndexIfNoneCreated),
+        ("testNoTagsPassedIntoBlogIndexIfNoneCreted", testNoTagsPassedIntoBlogIndexIfNoneCreted),
+        ("testUserPassedToBlogIndexIfUserPassedIn", testUserPassedToBlogIndexIfUserPassedIn),
+        ("testDisqusNamePassedToBlogIndexIfPassedIn", testDisqusNamePassedToBlogIndexIfPassedIn),
+        ("testTwitterHandlePassedToBlogIndexIfPassedIn", testTwitterHandlePassedToBlogIndexIfPassedIn),
         ]
     
     // MARK: - Properties
@@ -37,6 +51,8 @@ class LeafViewFactoryTests: XCTestCase {
     private let tagURI = URI(scheme: "https", host: "test.com", path: "tags/tatooine/")
     private var tagRequest: Request!
     private let postURI = URI(scheme: "https", host: "test.com", path: "posts/test-post/")
+    private let indexURI = URI(scheme: "https", host: "test.com", path: "/")
+    private var indexRequest: Request!
     
     // MARK: - Overrides
     
@@ -47,6 +63,7 @@ class LeafViewFactoryTests: XCTestCase {
         drop.database = database
         viewFactory = LeafViewFactory(drop: drop)
         tagRequest = try! Request(method: .get, uri: tagURI)
+        indexRequest = try! Request(method: .get, uri: indexURI)
         let printConsole = PrintConsole()
         let prepare = Prepare(console: printConsole, preparations: [BlogUser.self, BlogPost.self, BlogTag.self, Pivot<BlogPost, BlogTag>.self], database: database)
         do {
@@ -210,25 +227,128 @@ class LeafViewFactoryTests: XCTestCase {
     }
     
     func testBlogPageGetsImageUrlIfOneInPostMarkdown() throws {
-        var user = BlogUser(name: "Luke", username: "luke", password: "")
-        try user.save()
-        var postWithImage = TestDataBuilder.anyPostWithImage(author: user)
-        try postWithImage.save()
+       let (postWithImage, user) = try setupBlogPost()
         _ = try viewFactory.blogPostView(uri: postURI, post: postWithImage, author: user, user: nil, disqusName: nil, siteTwitterHandle: nil)
         
         XCTAssertNotNil((viewRenderer.capturedContext?["post_image"])?.string)
     }
     
     func testDescriptionOnBlogPostPageIsShortSnippetTextCleaned() throws {
-        var user = BlogUser(name: "Luke", username: "luke", password: "")
-        try user.save()
-        var postWithImage = TestDataBuilder.anyPostWithImage(author: user)
-        try postWithImage.save()
+        let (postWithImage, user) = try setupBlogPost()
         _ = try viewFactory.blogPostView(uri: postURI, post: postWithImage, author: user, user: nil, disqusName: nil, siteTwitterHandle: nil)
         
         let expectedDescription = "Welcome to SteamPress! SteamPress started out as an idea - after all, I was porting sites and backends over to Swift and would like to have a blog as well. Being early days for Server-Side Swift, and embracing Vapor, there wasn't anything available to put a blog on my site, so I did what any self-respecting engineer would do - I made one! Besides, what better way to learn a framework than build a blog!"
         
         XCTAssertEqual((viewRenderer.capturedContext?["post_description"])?.string?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines), expectedDescription)
+    }
+    
+    func testBlogPostPageGetsCorrectParameters() throws {
+        let (postWithImage, user) = try setupBlogPost()
+        _ = try viewFactory.blogPostView(uri: postURI, post: postWithImage, author: user, user: nil, disqusName: nil, siteTwitterHandle: nil)
+        
+        XCTAssertEqual(viewRenderer.capturedContext?["post"]?["title"]?.string, postWithImage.title)
+        XCTAssertEqual(viewRenderer.capturedContext?["author"]?["name"]?.string, user.name)
+        XCTAssertTrue(((viewRenderer.capturedContext?["blogPostPage"])?.bool) ?? false)
+        XCTAssertNil(viewRenderer.capturedContext?["user"])
+        XCTAssertNil(viewRenderer.capturedContext?["disqusName"])
+        XCTAssertNil(viewRenderer.capturedContext?["site_twitter_handle"])
+        XCTAssertNotNil((viewRenderer.capturedContext?["post_image"])?.string)
+        XCTAssertEqual(viewRenderer.capturedContext?["post_uri"]?.string, postURI.description)
+        XCTAssertEqual(viewRenderer.capturedContext?["site_uri"]?.string, "https://test.com:443")
+        XCTAssertEqual(viewRenderer.capturedContext?["post_uri_encoded"]?.string, postURI.description)
+    }
+    
+    func testUserPassedToBlogPostPageIfUserPassedIn() throws {
+        let (postWithImage, user) = try setupBlogPost()
+        _ = try viewFactory.blogPostView(uri: postURI, post: postWithImage, author: user, user: user, disqusName: nil, siteTwitterHandle: nil)
+        XCTAssertEqual(viewRenderer.capturedContext?["user"]?["name"]?.string, user.name)
+    }
+    
+    func testDisqusNamePassedToBlogPostPageIfPassedIn() throws {
+        let (postWithImage, user) = try setupBlogPost()
+        _ = try viewFactory.blogPostView(uri: postURI, post: postWithImage, author: user, user: nil, disqusName: "brokenhands", siteTwitterHandle: nil)
+        XCTAssertEqual(viewRenderer.capturedContext?["disqusName"]?.string, "brokenhands")
+    }
+    
+    func testTwitterHandlePassedToBlogPostPageIfPassedIn() throws {
+        let (postWithImage, user) = try setupBlogPost()
+        _ = try viewFactory.blogPostView(uri: postURI, post: postWithImage, author: user, user: nil, disqusName: nil, siteTwitterHandle: "brokenhandsio")
+        XCTAssertEqual(viewRenderer.capturedContext?["site_twitter_handle"]?.string, "brokenhandsio")
+    }
+    
+    func testBlogIndexPageGivenCorrectParameters() throws {
+        let (posts, tags, authors) = try setupBlogIndex()
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: posts.paginator(5, request: indexRequest), tags: tags, authors: authors, loggedInUser: nil, disqusName: nil, siteTwitterHandle: nil)
+
+        XCTAssertEqual(viewRenderer.capturedContext?["uri"]?.string, indexURI.description)
+        XCTAssertTrue((viewRenderer.capturedContext?["blogIndexPage"]?.bool) ?? false)
+        
+        XCTAssertEqual(viewRenderer.capturedContext?["posts"]?["data"]?.array?.count, posts.count)
+        XCTAssertEqual((viewRenderer.capturedContext?["posts"]?["data"]?.array?.first as? Node)?["title"]?.string, posts.first?.title)
+        XCTAssertEqual(viewRenderer.capturedContext?["tags"]?.array?.count, tags.count)
+        XCTAssertEqual((viewRenderer.capturedContext?["tags"]?.array?.first as? Node)?["name"]?.string, tags.first?.name)
+        XCTAssertEqual(viewRenderer.capturedContext?["authors"]?.array?.count, authors.count)
+        XCTAssertEqual((viewRenderer.capturedContext?["authors"]?.array?.first as? Node)?["name"]?.string, authors.first?.name)
+    }
+    
+    func testNoPostsPassedIntoBlogIndexIfNoneAvailable() throws {
+        let (_, tags, authors) = try setupBlogIndex()
+        let emptyBlogPosts: [BlogPost] = []
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: emptyBlogPosts.paginator(5, request: indexRequest), tags: tags, authors: authors, loggedInUser: nil, disqusName: nil, siteTwitterHandle: nil)
+        XCTAssertNil(viewRenderer.capturedContext?["posts"])
+    }
+    
+    func testNoAuthorsPassedIntoBlogIndexIfNoneCreated() throws {
+        let (posts, _, authors) = try setupBlogIndex()
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: posts.paginator(5, request: indexRequest), tags: [], authors: authors, loggedInUser: nil, disqusName: nil, siteTwitterHandle: nil)
+        XCTAssertNil(viewRenderer.capturedContext?["tags"])
+    }
+    
+    func testNoTagsPassedIntoBlogIndexIfNoneCreted() throws {
+        let (posts, tags, _) = try setupBlogIndex()
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: posts.paginator(5, request: indexRequest), tags: tags, authors: [], loggedInUser: nil, disqusName: nil, siteTwitterHandle: nil)
+        XCTAssertNil(viewRenderer.capturedContext?["authors"])
+    }
+    
+    func testUserPassedToBlogIndexIfUserPassedIn() throws {
+        let (posts, tags, authors) = try setupBlogIndex()
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: posts.paginator(5, request: indexRequest), tags: tags, authors: authors, loggedInUser: authors[0], disqusName: nil, siteTwitterHandle: nil)
+        XCTAssertEqual(viewRenderer.capturedContext?["user"]?["name"]?.string, authors.first?.name)
+    }
+    
+    func testDisqusNamePassedToBlogIndexIfPassedIn() throws {
+        let (posts, tags, authors) = try setupBlogIndex()
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: posts.paginator(5, request: indexRequest), tags: tags, authors: authors, loggedInUser: nil, disqusName: "brokenhands", siteTwitterHandle: nil)
+        XCTAssertEqual(viewRenderer.capturedContext?["disqusName"]?.string, "brokenhands")
+    }
+    
+    func testTwitterHandlePassedToBlogIndexIfPassedIn() throws {
+        let (posts, tags, authors) = try setupBlogIndex()
+        _ = try viewFactory.blogIndexView(uri: indexURI, paginatedPosts: posts.paginator(5, request: indexRequest), tags: tags, authors: authors, loggedInUser: nil, disqusName: nil, siteTwitterHandle: "brokenhandsio")
+        XCTAssertEqual(viewRenderer.capturedContext?["site_twitter_handle"]?.string, "brokenhandsio")
+    }
+    
+    private func setupBlogIndex() throws -> ([BlogPost], [BlogTag], [BlogUser]) {
+        var user1 = TestDataBuilder.anyUser()
+        try user1.save()
+        var user2 = TestDataBuilder.anyUser(name: "Han")
+        try user2.save()
+        var post1 = TestDataBuilder.anyPost(author: user1)
+        try post1.save()
+        var post2 = TestDataBuilder.anyPostWithImage(author: user2)
+        try post2.save()
+        var tag = BlogTag(name: "tatooine")
+        try tag.save()
+        try BlogTag.addTag(tag.name, to: post1)
+        return ([post1, post2], [tag], [user1, user2])
+    }
+    
+    private func setupBlogPost() throws -> (BlogPost, BlogUser) {
+        var user = BlogUser(name: "Luke", username: "luke", password: "")
+        try user.save()
+        var postWithImage = TestDataBuilder.anyPostWithImage(author: user)
+        try postWithImage.save()
+        return (postWithImage, user)
     }
     
     private func setupTagPage() throws -> BlogTag {
