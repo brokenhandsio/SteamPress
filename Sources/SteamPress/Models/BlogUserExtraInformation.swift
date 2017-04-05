350622ba97c8c3457c7c6b4b4209bb2f1262d9ca
Tim <0xtimc@gmail.com>
2017-04-05 08:52:49 +0100

    Might help if we try and append the right table...

--- Sources/SteamPress/Models/BlogUserExtraInformation.swift
+++ Sources/SteamPress/Models/BlogUserExtraInformation.swift
@@ -2,7 +2,7 @@ import Fluent
 
 struct BlogUserExtraInformation: Preparation {
     static func prepare(_ database: Database) throws {
-        try database.modify(BlogPost.entity) { users in
+        try database.modify(BlogUser.entity) { users in
             users.string("profile_picture", optional: true, default: nil)
             users.string("twitter_handle", optional: true, default: nil)
             users.custom("biography", type: "TEXT", optional: true, default: nil)
