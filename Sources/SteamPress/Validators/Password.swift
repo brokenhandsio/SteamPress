638d329e068b5baa357ef88de1a95fc3a4826f50
Tim <0xtimc@gmail.com>
2017-08-27 03:25:18 +0100

    No longer mandate password complexity

--- Sources/SteamPress/Validators/Password.swift
+++ Sources/SteamPress/Validators/Password.swift
@@ -7,25 +7,5 @@ struct PasswordValidator: Validator {
         if input.count < 8 {
             throw error("Password not long enough")
         }
-
-        // Check complexity
-        let upperCasePattern = "^(?=.*[A-Z]).*$"
-        let lowerCasePattern = "^(?=.*[a-z]).*$"
-        let numberPattern = "^(?=.*[0-9]).*$"
-        let symbolPattern = "^(?=.*[!@#%&-_=:;\"'<>,`~\\*\\?\\+\\[\\]\\(\\)\\{\\}\\^\\$\\|\\\\\\.\\/]).*$"
-
-        let patterns = [upperCasePattern, lowerCasePattern, numberPattern, symbolPattern]
-
-        var complexityStrength = 0
-
-        for pattern in patterns {
-            if input.range(of: pattern, options: .regularExpression) != nil {
-                complexityStrength += 1
-            }
-        }
-
-        if complexityStrength < 3 {
-            throw error("Password is not complex enough")
-        }
     }
 }
