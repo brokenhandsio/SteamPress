697f47b17af76250eebfa25f617dbf874c7d7ce6
Tim <0xtimc@gmail.com>
2019-11-16 14:09:37 +0100

    Remove old code from BlogTag

--- Sources/SteamPress/Models/BlogTag.swift
+++ Sources/SteamPress/Models/BlogTag.swift
@@ -15,39 +15,6 @@ public final class BlogTag: Codable {
 
 extension BlogTag: Content {}
 
-//
-//extension BlogTag: Parameterizable {}
-//
-
-// MARK: - Relations
-
-public extension BlogTag {
-
-//    public var posts: Siblings<BlogTag, BlogPost<Database>, BlogPostTagPivot<DatabaseType>> {
-//        return siblings()
-//    }
-//
-//    public func sortedPosts() throws -> Query<BlogPost> {
-//        return try posts.filter(BlogPost.Properties.published, true).sort(BlogPost.Properties.created, .descending)
-//    }
-//
-//    func deletePivot(for post: BlogPost) throws {
-//        try posts.remove(post)
-//    }
-
-//    static func addTag(_ name: String, to post: BlogPost<Database>, on conn: DatabaseConnectable) throws -> Future<Void> {
-//        return try BlogTag.query(on: conn).filter(\.name == name).first().flatMap(to: Void.self) { foundTag in
-//            if let exisitingTag = foundTag {
-//                return exisitingTag.posts.attach(post, on: conn).transform(to: Future.void)
-//            } else {
-//                return BlogTag(name: name).save(on: conn).flatMap(to: Void.self) { tag in
-//                    return tag.posts.attach(post, on: conn).transform(to: Future.void)
-//                }
-//            }
-//        }
-//    }
-}
-
 extension BlogTag {
     static func percentEncodedTagName(from name: String) throws -> String {
         guard let percentEncodedName = name.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
