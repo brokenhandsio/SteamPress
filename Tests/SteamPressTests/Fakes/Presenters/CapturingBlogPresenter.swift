a6a089206321d5aa1e4058d204d986b7fe46603a
Tim <0xtimc@gmail.com>
2019-12-13 16:47:45 +0000

    Add tests for page information for search and login and tags

--- Tests/SteamPressTests/Fakes/Presenters/CapturingBlogPresenter.swift
+++ Tests/SteamPressTests/Fakes/Presenters/CapturingBlogPresenter.swift
@@ -52,28 +52,31 @@ class CapturingBlogPresenter: BlogPresenter {
 
     private(set) var allTagsPageTags: [BlogTag]?
     private(set) var allTagsPagePostCount: [Int: Int]?
-    #warning("Page Info")
+    private(set) var allTagsPageInformation: BlogGlobalPageInformation?
     func allTagsView(on container: Container, tags: [BlogTag], tagPostCounts: [Int: Int], pageInformation: BlogGlobalPageInformation) -> EventLoopFuture<View> {
         self.allTagsPageTags = tags
         self.allTagsPagePostCount = tagPostCounts
+        self.allTagsPageInformation = pageInformation
         return TestDataBuilder.createFutureView(on: container)
     }
 
     private(set) var tag: BlogTag?
     private(set) var tagPosts: [BlogPost]?
-    #warning("Page Info")
+    private(set) var tagPageInformation: BlogGlobalPageInformation?
     func tagView(on container: Container, tag: BlogTag, posts: [BlogPost], pageInformation: BlogGlobalPageInformation) -> EventLoopFuture<View> {
         self.tag = tag
         self.tagPosts = posts
+        self.tagPageInformation = pageInformation
         return TestDataBuilder.createFutureView(on: container)
     }
 
     private(set) var searchPosts: [BlogPost]?
     private(set) var searchTerm: String?
-    #warning("Page Info")
+    private(set) var searchPageInformation: BlogGlobalPageInformation?
     func searchView(on container: Container, posts: [BlogPost], searchTerm: String?, pageInformation: BlogGlobalPageInformation) -> EventLoopFuture<View> {
         self.searchPosts = posts
         self.searchTerm = searchTerm
+        self.searchPageInformation = pageInformation
         return TestDataBuilder.createFutureView(on: container)
     }
 
@@ -82,13 +85,14 @@ class CapturingBlogPresenter: BlogPresenter {
     private(set) var loginUsername: String?
     private(set) var loginUsernameError: Bool?
     private(set) var loginPasswordError: Bool?
-    #warning("Page Info")
+    private(set) var loginPageInformation: BlogGlobalPageInformation?
     func loginView(on container: Container, loginWarning: Bool, errors: [String]?, username: String?, usernameError: Bool, passwordError: Bool, pageInformation: BlogGlobalPageInformation) -> EventLoopFuture<View> {
         self.loginWarning = loginWarning
         self.loginErrors = errors
         self.loginUsername = username
         self.loginUsernameError = usernameError
         self.loginPasswordError = passwordError
+        self.loginPageInformation = pageInformation
         return TestDataBuilder.createFutureView(on: container)
     }
 }
