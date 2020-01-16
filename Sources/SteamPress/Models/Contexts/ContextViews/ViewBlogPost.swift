c70c3e6664a942b505cb3d931eed51a5a43abb5e
Tim <0xtimc@gmail.com>
2020-01-16 11:26:48 +0800

    Fix last edited display on admin page

--- Sources/SteamPress/Models/Contexts/ContextViews/ViewBlogPost.swift
+++ Sources/SteamPress/Models/Contexts/ContextViews/ViewBlogPost.swift
@@ -12,11 +12,18 @@ struct ViewBlogPost: Encodable {
     var longSnippet: String
     var createdDateLong: String
     var createdDateNumeric: String
+    var lastEditedDateNumeric: String?
     var authorName: String
 }
 
 extension BlogPost {
     func toViewPost(authorName: String, longFormatter: LongPostDateFormatter, numericFormatter: NumericPostDateFormatter) -> ViewBlogPost {
-        ViewBlogPost(blogID: self.blogID, title: self.title, contents: self.contents, author: self.author, created: self.created, lastEdited: self.lastEdited, slugUrl: self.slugUrl, published: self.published, longSnippet: self.longSnippet(), createdDateLong: longFormatter.formatter.string(from: self.created), createdDateNumeric: numericFormatter.formatter.string(from: self.created), authorName: authorName)
+        let lastEditedNumeric: String?
+        if let lastEdited = self.lastEdited {
+            lastEditedNumeric = numericFormatter.formatter.string(from: lastEdited)
+        } else {
+            lastEditedNumeric = nil
+        }
+        return ViewBlogPost(blogID: self.blogID, title: self.title, contents: self.contents, author: self.author, created: self.created, lastEdited: self.lastEdited, slugUrl: self.slugUrl, published: self.published, longSnippet: self.longSnippet(), createdDateLong: longFormatter.formatter.string(from: self.created), createdDateNumeric: numericFormatter.formatter.string(from: self.created), lastEditedDateNumeric: lastEditedNumeric, authorName: authorName)
     }
 }
