5c1d0634f2d2c4760dcccffc23e428dcce0f9fa2
Tim <0xtimc@gmail.com>
2020-03-26 15:26:21 +0000

    Update manifest to 5.2

--- Package.swift
+++ Package.swift
@@ -1,20 +1,28 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "SteamPress",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(name: "SteamPress", targets: ["SteamPress"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(name: "Vapor", url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.0.0"),
-        .package(url: "https://github.com/vapor-community/markdown.git", from: "0.4.0"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+        .package(name: "SwiftMarkdown", url: "https://github.com/vapor-community/markdown.git", from: "0.4.0"),
+        .package(name: "Auth", url: "https://github.com/vapor/auth.git", from: "2.0.0"),
     ],
     targets: [
-        .target(name: "SteamPress", dependencies: ["Vapor", "SwiftSoup", "SwiftMarkdown", "Authentication"]),
+        .target(name: "SteamPress", dependencies: [
+            "Vapor",
+            "SwiftSoup",
+            "SwiftMarkdown",
+            .product(name: "Authentication", package: "Auth")
+        ]),
         .testTarget(name: "SteamPressTests", dependencies: ["SteamPress"]),
     ]
 )
