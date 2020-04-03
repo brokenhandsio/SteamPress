ce9682e66a915e0b72fcdef22f1c10955cc4a4b7
Tim <0xtimc@gmail.com>
2020-04-03 17:42:51 +0100

    Get the TestWorld compiling

--- Tests/SteamPressTests/Helpers/TestWorld.swift
+++ Tests/SteamPressTests/Helpers/TestWorld.swift
@@ -4,11 +4,12 @@ import Vapor
 struct TestWorld {
 
     static func create(path: String? = nil, postsPerPage: Int = 10, feedInformation: FeedInformation = FeedInformation(), enableAuthorPages: Bool = true, enableTagPages: Bool = true, passwordHasherToUse: PasswordHasherChoice = .plaintext, randomNumberGenerator: StubbedRandomNumberGenerator = StubbedRandomNumberGenerator(numberToReturn: 666)) throws -> TestWorld {
-        let repository = InMemoryRepository()
-        let blogPresenter = CapturingBlogPresenter()
-        let blogAdminPresenter = CapturingAdminPresenter()
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let repository = InMemoryRepository(eventLoop: eventLoopGroup.next())
+        let blogPresenter = CapturingBlogPresenter(eventLoop: eventLoopGroup.next())
+        let blogAdminPresenter = CapturingAdminPresenter(eventLoop: eventLoopGroup.next())
         let application = try TestWorld.getSteamPressApp(repository: repository, path: path, postsPerPage: postsPerPage, feedInformation: feedInformation, blogPresenter: blogPresenter, adminPresenter: blogAdminPresenter, enableAuthorPages: enableAuthorPages, enableTagPages: enableTagPages, passwordHasherToUse: passwordHasherToUse, randomNumberGenerator: randomNumberGenerator)
-        let context = Context(app: application, repository: repository, blogPresenter: blogPresenter, blogAdminPresenter: blogAdminPresenter, path: path)
+        let context = Context(app: application, repository: repository, blogPresenter: blogPresenter, blogAdminPresenter: blogAdminPresenter, path: path, eventLoopGroup: eventLoopGroup)
         unsetenv("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
         unsetenv("BLOG_SITE_TWITTER_HANDLE")
         unsetenv("BLOG_DISQUS_NAME")
@@ -28,11 +29,13 @@ struct TestWorld {
         let blogPresenter: CapturingBlogPresenter
         let blogAdminPresenter: CapturingAdminPresenter
         let path: String?
+        let eventLoopGroup: EventLoopGroup
     }
     
-    #warning("Remove")
+    #warning("Remove or rename")
     // To work around Vapor 3 dodgy lifecycle mess
     mutating func tryAsHardAsWeCanToShutdownApplication() throws {
+        try context.eventLoopGroup.syncShutdownGracefully()
 //        struct ApplicationDidNotGoAway: Error {
 //            var description: String
 //        }
